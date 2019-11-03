e50e55092f7632a789c18fa799b4847c019eef90
dpj <1115957667@qq.com>
2019-11-03 09:49:24 +0800

    Revert "update"
    This reverts commit ba932cec5311d8c317f6674024aab1f22f80fb70.

--- main.py
+++
@@ -1,9 +0,0 @@
-from parameters import *
-from model import *
-from train import *
-
-if __name__ == '__main__':
-    net = ResNet(ResidualBlock)
-    net.loadIfExist()
-    # print(net.named_parameters())
-    train(net)
