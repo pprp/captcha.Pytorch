5b540970950702d0b8ea87c4ef13bbcc8921a0ad
pprp <1115957667@qq.com>
2019-12-05 12:17:50 +0800

    update to latest version

--- config/parameters.py
+++ config/parameters.py
@@ -4,7 +4,8 @@ charNumber = 4
 ImageWidth = 120
 ImageHeight = 40
 
-learningRate = 3.5e-4  # 1e-2
+learningRate = 3e-4  # 3e-4 #3.5e-4  # 1e-2
 totalEpoch = 600
 batchSize = 64
 printCircle = 10
+
--- main.py
+++
@@ -1,9 +0,0 @@
-from parameters import *
-from model import *
-from train import *
-
-if __name__ == '__main__':
-    net = RES50(ResidualBlock)
-    # net = CaptchaNet()
-    net.loadIfExist()
-    train(net)
